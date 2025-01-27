432b63a11758ed2b1ab51be2e3767fb5246514f8
Marius 't Hart <thartbm@gmail.com>
2025-01-27 11:26:06 -0500

    wrote a function to download the data from OSF

---
+++ R/data.R
@@ -0,0 +1,19 @@
+
+
+# download data from OSF:
+
+getData <- function() {
+  
+  Reach::downloadOSFdata(repository  = 'm5dt4',
+                         filelist    = list('data/'=c('control_aligned.zip',
+                                                      'control_rotated.zip',
+                                                      'handview_aligned.zip',
+                                                      'handview_rotated.zip',
+                                                      'cursorjump_aligned.zip',
+                                                      'cursorjump_rotated.zip')  ),
+                         folder      = 'data/',
+                         overwrite   = TRUE,
+                         unzip       = TRUE,
+                         removezips  = FALSE)
+  
+}
