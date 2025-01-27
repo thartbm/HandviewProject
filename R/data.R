8ca4876db49c37fe26727a864effd3c72b8882a9
Marius 't Hart <thartbm@gmail.com>
2025-01-27 16:02:18 -0500

    set options as I like them

--- R/data.R
+++ R/data.R
@@ -5,15 +5,16 @@
 getData <- function() {
   
   Reach::downloadOSFdata(repository  = 'm5dt4',
-                         filelist    = list('data/'=c('control_aligned.zip',
-                                                      'control_rotated.zip',
-                                                      'handview_aligned.zip',
-                                                      'handview_rotated.zip',
-                                                      'cursorjump_aligned.zip',
-                                                      'cursorjump_rotated.zip')  ),
+                         filelist    = list('data/' = c('control_aligned.zip',
+                                                        'control_rotated.zip',
+                                                        'handview_aligned.zip',
+                                                        'handview_rotated.zip',
+                                                        'cursorjump_aligned.zip',
+                                                        'cursorjump_rotated.zip'
+                                                                 )  ),
                          folder      = 'data/',
                          overwrite   = TRUE,
                          unzip       = TRUE,
-                         removezips  = FALSE)
+                         removezips  = TRUE)
   
 }
