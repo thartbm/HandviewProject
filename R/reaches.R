b5e66c6290a7b7e1ea412ae46f3954666cfe8158
Marius 't Hart <thartbm@gmail.com>
2025-02-10 11:22:24 -0500

    start code for processing reach deviations

--- R/reaches.R
+++ R/reaches.R
@@ -8,6 +8,8 @@ getParticipantTraining <- function(group, participant) {
   RO_df <- read.csv( file = RO_file,
                      stringsAsFactors = F)
   
+  return( list('aligned'=AL_df,
+               'rotated'=RO_df) )
   
 }
 
@@ -16,12 +18,74 @@ getGroupTraining <- function(group) {
   
   participants <- groupParticipants(group = group)
   
-  for (participant in participants) {
+  # print(participants)
+  
+  for (participant in participants[c(1:3)]) {
     
-    participant_df <- getParticipantTraining( group = group,
-                                              participant = participant)
+    participant_df <- getParticipantTraining( group       = group,
+                                              participant = participant )
+    
+    # print(participant_df)
+    
+    baseline <- getBaseline( df = participant_df[['aligned']] )
     
     
   }
+  
+}
+
+getBaseline <- function(df) {
+  
+  # str(df)
+  df <- df[which(df$trial_num %in% c(31:45)),]
+  # str(df)
+  
+  trialnos <- unique(df$trial_num)
+  
+  for (trial in trialnos) {
+    
+    tdf <- df[which(df$trial_num == trial),]
+    
+    reachdev <- getReachDeviation(tdf)
+    print(reachdev)
+    
+  }
+  
+}
+
+getReachDeviation <- function(df) {
+  
+  
+  target <- df$targetangle_deg[1]
+  
+  X <- df$handx_cm
+  Y <- df$handy_cm
+  
+  # at 1/4 the target distance
+  # target distance was 10 cm
+  # 2.5 cm
+  
+  distances <- sqrt(X^2 + Y^2)
+  idx <- which(distances > 2.5)[1]
+  
+  # print(idx)
+  x <- X[idx]
+  y <- Y[idx]
+  
+  th <- (-1*target/180) * pi
+  R <- t(matrix(data=c(cos(th),sin(th),-sin(th),cos(th)),nrow=2,ncol=2))
+  
+  # rotate the coordinates, add the origin back in
+  norm_sample <- matrix(data=c(x,y),ncol=2) %*% R
+  
+  # print(norm_sample)
+  
+  reachdev <- (atan2(norm_sample[2], norm_sample[1]) / pi) * 180
+  
+  print(reachdev)
+  
+  return(c('trial_num' = df$trial_num[1],
+           'targetangle_deg'=target,
+           'reachdeviation_deg'=reachdev))
   
 }
